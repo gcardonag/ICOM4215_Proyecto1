24d4077616d43acb5f49e7966845483fb3244697
Gabriel Cardona <gcardonagarcia@gmail.com>
2012-09-28 00:16:31 -0400

    Adding placeholder folder in order to properly upload "src" folder.

---
+++ ICOM4215_Proyecto1/src/placeholder/Placeholder.java
@@ -0,0 +1,16 @@
+package placeholder;
+
+public class Placeholder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Debido a que git no reconoce directorios vacios, estoy a–adiendo esta clase "Placeholder"
+		//dentro de un paquete "placeholder" para que puedan accesar el folder "src".
+
+	}
+
+}
