9e38e408233e72f79d43416b28900b6d31890515
Gabriel Cardona <gcardonagarcia@gmail.com>
2012-10-03 10:33:51 -0400

    Modifying accumulator AND and adding accumulator OR.

--- ICOM4215_Proyecto1/src/accumulator/Accumulator.java
+++ ICOM4215_Proyecto1/src/accumulator/Accumulator.java
@@ -6,6 +6,7 @@ public class Accumulator {
 	public Accumulator() {
 		acc_value = "00000000";
 	}
+	
 	/**
 	 * Receives a string consisting of a binary value and performs a logical AND with the
 	 * accumulator, storing the result back in the accumulator.
@@ -16,14 +17,36 @@ public class Accumulator {
 		String result = "";
 		for(int i=0; i<8; i++)
 		{
-			int acc_bit = Integer.parseInt(acc_value.substring(i, i+1));
-			int reg_bit = Integer.parseInt(reg_value.substring(i,i+1));
-			if(acc_bit == 1 && reg_bit == 1)
+			String acc_bit = acc_value.substring(i, i+1);
+			String reg_bit = reg_value.substring(i,i+1);
+			if(acc_bit.equals("1") && reg_bit.equals("1"))
 			{
 				result = result + "1";
 			}
 			else
 				result = result + "0";
+		}
+		acc_value = result;
+	}
+	
+	/**
+	 * Receives a string consisting of a binary value and performs a logical OR with the
+	 * accumulator, storing the result back in the accumulator.
+	 * 
+	 * @param reg_value
+	 */
+	public void acc_or(String reg_value){
+		String result = "";
+		for(int i=0; i<8; i++)
+		{
+			String acc_bit = acc_value.substring(i, i+1);
+			String reg_bit = reg_value.substring(i,i+1);
+			if(acc_bit.equals("0") && reg_bit.equals("0"))
+			{
+				result = result + "0";
+			}
+			else
+				result = result + "1";
 		}
 		acc_value = result;
 	}
