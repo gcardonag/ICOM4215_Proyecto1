cb4869c3b8275829c9ca73080a55ddc87cccf6b7
Gabriel Cardona <gcardonagarcia@gmail.com>
2012-10-07 16:26:09 -0400

    Added branching operations and Status Register to CPU class.

--- ICOM4215_Proyecto1/src/cpu/CPU.java
+++ ICOM4215_Proyecto1/src/cpu/CPU.java
@@ -4,6 +4,7 @@ public class CPU {
 	
 	public static StatusRegister SR = new StatusRegister();
 	
+<<<<<<< HEAD
 	// Registers
 	public static String R0, R1, R2, R3, R4, R5, R6, R7;
 	
@@ -14,6 +15,53 @@ public class CPU {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+=======
+	public static String PC = "00000000";
+	
+	public CPU()
+	{
+		R0 = "00000000";
+		R1 = "00000000";
+		R2 = "00000000";
+		R3 = "00000000";
+		R4 = "00000000";
+		R5 = "00000000";
+		R6 = "00000000";
+		R7 = "00000000";
 	}
-
+	
+	public void step(String hex_value)
+	{
+		
+	}
+	
+	private void brz()
+	{
+		if(SR.zero.equals("1"))
+			PC = R7;
+	}
+	
+	private void brc()
+	{
+		if(SR.carry.equals("1"))
+			PC = R7;
+	}
+	
+	private void brn()
+	{
+		if(SR.negative.equals("1"))
+			PC = R7;
+	}
+	
+	private void bro()
+	{
+		if(SR.overflow.equals("1"))
+			PC = R7;
+	}
+	
+	private void stop()
+	{
+		PC = null;
+>>>>>>> Added branching operations and Status Register to CPU class.
+	}
 }
