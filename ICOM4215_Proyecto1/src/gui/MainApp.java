ce6fbf3647dac3cea7e87deedc322d5873ffed36
Hector Ayala <hector.ayala6@upr.edu>
2012-10-10 19:36:12 -0400

    Main GUI class with working Keyboard Input

--- ICOM4215_Proyecto1/src/gui/MainApp.java
+++ ICOM4215_Proyecto1/src/gui/MainApp.java
@@ -4,23 +4,43 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import javax.swing.JTextArea;
-import javax.swing.JScrollBar;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JPanel;
 
-public class MainApp implements ActionListener {
+public class MainApp implements ActionListener, KeyListener {
 
 	static private File file = new File("src/prog.txt");
 	static private Scanner scanner;
 
-	private JFrame frmHghSrcSimulator;
+	private JFrame mainFrame;
+	private JTextArea tA_R3;
+	private JTextArea tA_R2;
+	private JTextArea tA_R6;
+	private JTextArea tA_SR;
+	private JTextArea tA_memory;
+	private JTextArea tA_R5;
+	private JTextArea tA_R7;
+	private JTextArea tA_Acc;
+	private JTextArea tA_R1;
+	private JTextArea tA_Keyboard;
+	private JTextArea tA_PC;
+	private JTextArea tA_R4;
+	private JTextArea tA_VB;
+	private JTextArea tA_Display;
+	private JTextArea tA_R0;
+	private JTextArea tA_IR;
 
 	/**
 	 * Launch the application.
@@ -30,7 +50,7 @@ public class MainApp implements ActionListener {
 			public void run() {
 				try {
 					MainApp window = new MainApp();
-					window.frmHghSrcSimulator.setVisible(true);
+					window.mainFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -57,195 +77,204 @@ public class MainApp implements ActionListener {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmHghSrcSimulator = new JFrame();
-		frmHghSrcSimulator.setTitle("HGH SRC Simulator");
-		frmHghSrcSimulator.setBounds(100, 100, 435, 480);
-		frmHghSrcSimulator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmHghSrcSimulator.getContentPane().setLayout(null);
+		mainFrame = new JFrame();
+		mainFrame.setTitle("HGH SRC Simulator");
+		mainFrame.setBounds(100, 100, 435, 480);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.getContentPane().setLayout(null);
 
-		JLabel lblIr = new JLabel("IR");
-		lblIr.setHorizontalAlignment(SwingConstants.CENTER);
-		lblIr.setBounds(10, 21, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblIr);
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 427, 453);
+		mainFrame.getContentPane().add(panel);
+		panel.setLayout(null);
+		panel.setFocusable(true);
+		panel.requestFocusInWindow();
 
-		JLabel lblPc = new JLabel("PC");
-		lblPc.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPc.setBounds(10, 52, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblPc);
+		JLabel label = new JLabel("IR");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setBounds(10, 20, 30, 20);
+		panel.add(label);
 
-		JLabel lblAcc = new JLabel("Acc");
-		lblAcc.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAcc.setBounds(10, 83, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblAcc);
+		JLabel label_1 = new JLabel("PC");
+		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+		label_1.setBounds(10, 50, 30, 20);
+		panel.add(label_1);
 
-		JTextArea tA_IR = new JTextArea();
+		JLabel label_2 = new JLabel("Acc");
+		label_2.setHorizontalAlignment(SwingConstants.CENTER);
+		label_2.setBounds(10, 80, 30, 20);
+		panel.add(label_2);
+
+		JLabel label_3 = new JLabel("Registers");
+		label_3.setHorizontalAlignment(SwingConstants.CENTER);
+		label_3.setBounds(54, 141, 60, 20);
+		panel.add(label_3);
+
+		JLabel label_4 = new JLabel("R0");
+		label_4.setHorizontalAlignment(SwingConstants.CENTER);
+		label_4.setBounds(10, 170, 30, 20);
+		panel.add(label_4);
+
+		JLabel label_5 = new JLabel("R1");
+		label_5.setHorizontalAlignment(SwingConstants.CENTER);
+		label_5.setBounds(10, 200, 30, 20);
+		panel.add(label_5);
+
+		JLabel label_6 = new JLabel("R2");
+		label_6.setHorizontalAlignment(SwingConstants.CENTER);
+		label_6.setBounds(10, 230, 30, 20);
+		panel.add(label_6);
+
+		JLabel label_7 = new JLabel("R3");
+		label_7.setHorizontalAlignment(SwingConstants.CENTER);
+		label_7.setBounds(10, 260, 30, 20);
+		panel.add(label_7);
+
+		JLabel label_8 = new JLabel("R4");
+		label_8.setHorizontalAlignment(SwingConstants.CENTER);
+		label_8.setBounds(10, 290, 30, 20);
+		panel.add(label_8);
+
+		JLabel label_9 = new JLabel("R5");
+		label_9.setHorizontalAlignment(SwingConstants.CENTER);
+		label_9.setBounds(10, 320, 30, 20);
+		panel.add(label_9);
+
+		JLabel label_10 = new JLabel("R6");
+		label_10.setHorizontalAlignment(SwingConstants.CENTER);
+		label_10.setBounds(10, 350, 30, 20);
+		panel.add(label_10);
+
+		JLabel label_11 = new JLabel("R7");
+		label_11.setHorizontalAlignment(SwingConstants.CENTER);
+		label_11.setBounds(10, 380, 30, 20);
+		panel.add(label_11);
+
+		JLabel label_12 = new JLabel("SR");
+		label_12.setHorizontalAlignment(SwingConstants.CENTER);
+		label_12.setBounds(10, 110, 30, 20);
+		panel.add(label_12);
+
+		JLabel label_13 = new JLabel("Memory");
+		label_13.setHorizontalAlignment(SwingConstants.CENTER);
+		label_13.setBounds(326, 85, 60, 20);
+		panel.add(label_13);
+
+		JLabel label_14 = new JLabel("Vector Buffer");
+		label_14.setHorizontalAlignment(SwingConstants.CENTER);
+		label_14.setBounds(167, 132, 80, 20);
+		panel.add(label_14);
+
+		JLabel label_15 = new JLabel("Keyboard");
+		label_15.setHorizontalAlignment(SwingConstants.CENTER);
+		label_15.setBounds(166, 186, 80, 20);
+		panel.add(label_15);
+
+		JLabel label_16 = new JLabel("Display");
+		label_16.setHorizontalAlignment(SwingConstants.CENTER);
+		label_16.setBounds(166, 261, 80, 20);
+		panel.add(label_16);
+
+		JButton b_STEP = new JButton("STEP");
+		b_STEP.setToolTipText("Run one step of the program.");
+		b_STEP.setActionCommand("STEP");
+		b_STEP.setBounds(348, 31, 69, 39);
+		panel.add(b_STEP);
+
+		JButton b_RUN = new JButton("RUN");
+		b_RUN.setToolTipText("Run the remainder of the program.");
+		b_RUN.setActionCommand("RUN");
+		b_RUN.setBounds(251, 31, 69, 39);
+		panel.add(b_RUN);
+
+		tA_IR = new JTextArea();
 		tA_IR.setEditable(false);
-		tA_IR.setBounds(50, 20, 146, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_IR);
+		tA_IR.setBounds(44, 20, 146, 20);
+		panel.add(tA_IR);
 
-		JTextArea tA_PC = new JTextArea();
+		tA_PC = new JTextArea();
 		tA_PC.setEditable(false);
-		tA_PC.setBounds(50, 51, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_PC);
+		tA_PC.setBounds(44, 50, 86, 20);
+		panel.add(tA_PC);
 
-		JTextArea tA_Acc = new JTextArea();
+		tA_Acc = new JTextArea();
 		tA_Acc.setEditable(false);
-		tA_Acc.setBounds(50, 82, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_Acc);
+		tA_Acc.setBounds(44, 80, 86, 20);
+		panel.add(tA_Acc);
 
-		JLabel lblRegisters = new JLabel("Registers");
-		lblRegisters.setHorizontalAlignment(SwingConstants.CENTER);
-		lblRegisters.setBounds(60, 157, 60, 20);
-		frmHghSrcSimulator.getContentPane().add(lblRegisters);
-
-		JLabel lblR = new JLabel("R0");
-		lblR.setHorizontalAlignment(SwingConstants.CENTER);
-		lblR.setBounds(10, 189, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblR);
-
-		JTextArea tA_R0 = new JTextArea();
-		tA_R0.setEditable(false);
-		tA_R0.setBounds(50, 188, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_R0);
-
-		JTextArea tA_R1 = new JTextArea();
-		tA_R1.setEditable(false);
-		tA_R1.setBounds(50, 220, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_R1);
-
-		JTextArea tA_R2 = new JTextArea();
-		tA_R2.setEditable(false);
-		tA_R2.setBounds(50, 252, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_R2);
-
-		JTextArea tA_R3 = new JTextArea();
-		tA_R3.setEditable(false);
-		tA_R3.setBounds(50, 284, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_R3);
-
-		JTextArea tA_R4 = new JTextArea();
-		tA_R4.setEditable(false);
-		tA_R4.setBounds(50, 315, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_R4);
-
-		JTextArea tA_R5 = new JTextArea();
-		tA_R5.setEditable(false);
-		tA_R5.setBounds(50, 347, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_R5);
-
-		JTextArea tA_R6 = new JTextArea();
-		tA_R6.setEditable(false);
-		tA_R6.setBounds(50, 379, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_R6);
-
-		JTextArea tA_R7 = new JTextArea();
-		tA_R7.setEditable(false);
-		tA_R7.setBounds(50, 411, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_R7);
-
-		JTextArea tA_SR = new JTextArea();
+		tA_SR = new JTextArea();
 		tA_SR.setEditable(false);
-		tA_SR.setBounds(50, 114, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_SR);
+		tA_SR.setBounds(44, 110, 86, 20);
+		panel.add(tA_SR);
 
-		JTextArea tA_VB = new JTextArea();
+		tA_R0 = new JTextArea();
+		tA_R0.setEditable(false);
+		tA_R0.setBounds(44, 170, 86, 20);
+		panel.add(tA_R0);
+
+		tA_R1 = new JTextArea();
+		tA_R1.setEditable(false);
+		tA_R1.setBounds(44, 200, 86, 20);
+		panel.add(tA_R1);
+
+		tA_R2 = new JTextArea();
+		tA_R2.setEditable(false);
+		tA_R2.setBounds(44, 230, 86, 20);
+		panel.add(tA_R2);
+
+		tA_R3 = new JTextArea();
+		tA_R3.setEditable(false);
+		tA_R3.setBounds(44, 260, 86, 20);
+		panel.add(tA_R3);
+
+		tA_R4 = new JTextArea();
+		tA_R4.setEditable(false);
+		tA_R4.setBounds(44, 290, 86, 20);
+		panel.add(tA_R4);
+
+		tA_R5 = new JTextArea();
+		tA_R5.setEditable(false);
+		tA_R5.setBounds(44, 320, 86, 20);
+		panel.add(tA_R5);
+
+		tA_R6 = new JTextArea();
+		tA_R6.setEditable(false);
+		tA_R6.setBounds(44, 350, 86, 20);
+		panel.add(tA_R6);
+
+		tA_R7 = new JTextArea();
+		tA_R7.setEditable(false);
+		tA_R7.setBounds(44, 380, 86, 20);
+		panel.add(tA_R7);
+
+		tA_VB = new JTextArea();
 		tA_VB.setEditable(false);
-		tA_VB.setBounds(173, 114, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_VB);
+		tA_VB.setBounds(166, 155, 86, 20);
+		panel.add(tA_VB);
 
-		JTextArea tA_Keyboard = new JTextArea();
+		tA_Keyboard = new JTextArea();
 		tA_Keyboard.setEditable(false);
-		tA_Keyboard.setBounds(173, 164, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_Keyboard);
+		tA_Keyboard.setBounds(166, 219, 86, 20);
+		panel.add(tA_Keyboard);
 
-		JTextArea tA_Display = new JTextArea();
+		tA_Display = new JTextArea();
 		tA_Display.setEditable(false);
-		tA_Display.setBounds(173, 214, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(tA_Display);
+		tA_Display.setBounds(166, 290, 86, 20);
+		panel.add(tA_Display);
 
-		JTextArea tA_memory = new JTextArea();
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(299, 113, 118, 317);
+		panel.add(scrollPane);
+
+		tA_memory = new JTextArea();
 		tA_memory.setEditable(false);
-		tA_memory.setBounds(296, 114, 90, 318);
-		frmHghSrcSimulator.getContentPane().add(tA_memory);
+		scrollPane.setViewportView(tA_memory);
 
-		JLabel lblR_1 = new JLabel("R1");
-		lblR_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblR_1.setBounds(10, 221, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblR_1);
-
-		JLabel lblR_2 = new JLabel("R2");
-		lblR_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblR_2.setBounds(10, 253, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblR_2);
-
-		JLabel lblR_3 = new JLabel("R3");
-		lblR_3.setHorizontalAlignment(SwingConstants.CENTER);
-		lblR_3.setBounds(10, 285, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblR_3);
-
-		JLabel lblR_4 = new JLabel("R4");
-		lblR_4.setHorizontalAlignment(SwingConstants.CENTER);
-		lblR_4.setBounds(10, 316, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblR_4);
-
-		JLabel lblR_5 = new JLabel("R5");
-		lblR_5.setHorizontalAlignment(SwingConstants.CENTER);
-		lblR_5.setBounds(10, 348, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblR_5);
-
-		JLabel lblR_6 = new JLabel("R6");
-		lblR_6.setHorizontalAlignment(SwingConstants.CENTER);
-		lblR_6.setBounds(10, 380, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblR_6);
-
-		JLabel lblR_7 = new JLabel("R7");
-		lblR_7.setHorizontalAlignment(SwingConstants.CENTER);
-		lblR_7.setBounds(10, 412, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblR_7);
-
-		JLabel lblSr = new JLabel("SR");
-		lblSr.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSr.setBounds(10, 115, 30, 20);
-		frmHghSrcSimulator.getContentPane().add(lblSr);
-
-		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.setBounds(386, 114, 16, 318);
-		frmHghSrcSimulator.getContentPane().add(scrollBar);
-
-		JLabel lblMemory = new JLabel("Memory");
-		lblMemory.setHorizontalAlignment(SwingConstants.CENTER);
-		lblMemory.setBounds(316, 86, 60, 20);
-		frmHghSrcSimulator.getContentPane().add(lblMemory);
-
-		JLabel lblVectorBuffer = new JLabel("Vector Buffer");
-		lblVectorBuffer.setHorizontalAlignment(SwingConstants.CENTER);
-		lblVectorBuffer.setBounds(173, 89, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(lblVectorBuffer);
-
-		JLabel lblKeyboard = new JLabel("Keyboard");
-		lblKeyboard.setHorizontalAlignment(SwingConstants.CENTER);
-		lblKeyboard.setBounds(173, 139, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(lblKeyboard);
-
-		JLabel lblDisplay = new JLabel("Display");
-		lblDisplay.setHorizontalAlignment(SwingConstants.CENTER);
-		lblDisplay.setBounds(173, 189, 80, 20);
-		frmHghSrcSimulator.getContentPane().add(lblDisplay);
-
-		JButton btnSTEP = new JButton("STEP");
-		btnSTEP.setBounds(330, 21, 69, 39);
-		frmHghSrcSimulator.getContentPane().add(btnSTEP);
-		btnSTEP.setActionCommand("STEP");
-		btnSTEP.setToolTipText("Run one step of the program.");
-
-		JButton btnRUN = new JButton("RUN");
-		btnRUN.setBounds(237, 21, 69, 39);
-		frmHghSrcSimulator.getContentPane().add(btnRUN);
-		btnRUN.setActionCommand("RUN");
-		btnRUN.setToolTipText("Run the remainder of the program.");
-
-		btnSTEP.addActionListener(this);
-		btnRUN.addActionListener(this);
+		panel.addKeyListener(this);
+		b_STEP.addActionListener(this);
+		b_RUN.addActionListener(this);
 
 	}
 
@@ -254,13 +283,40 @@ public class MainApp implements ActionListener {
 		if("RUN".equals(e.getActionCommand())){
 			while(scanner.hasNextLine()){
 				System.out.println(scanner.next());
+				mainFrame.repaint();
 			}
 		}
 		else{
 			if(scanner.hasNextLine()){
-				System.out.println(scanner.next());
+				String temp = scanner.next();
+				tA_IR.setText(temp);
+				System.out.println(temp);
 			}
 		}
+
+	}
+
+	public void update(){
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		System.out.println("---");
+		System.out.println("Char " + e.getKeyChar());
+		System.out.println("Key Code " + e.getKeyCode());
+		System.out.println("ID " + e.getID());
+		System.out.println("ASCII " + (int) e.getKeyChar());
 
 	}
 
